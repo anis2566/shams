d618e9c08463a6123781b70e62d44b4e0d65fc49
shams256 <shamspublication57@gmail.com>
2024-12-12 18:05:02 +0600

    faf

--- app/dashboard/books/page.tsx
+++ app/dashboard/books/page.tsx
@@ -20,9 +20,10 @@ import {
 
 import { ContentLayout } from "../_components/content-layout";
 import { db } from "@/lib/prisma";
-import { CustomPagination } from "@/components/custom-pagination";
+import { CustomPagination } from "@/components/custom-pagination-2";
 import { BookList } from "./_components/book-list";
 import { Header } from "./_components/header";
+import { BookOpen } from "lucide-react";
 
 export const metadata: Metadata = {
     title: "Shams Publication | Books",
@@ -49,7 +50,7 @@ const Books = async ({ searchParams }: Props) => {
     const itemsPerPage = parseInt(perPage, 10);
     const currentPage = parseInt(page, 10);
 
-    const [books, totalBooks] = await Promise.all([
+    const [books, totalBooks, totalbook, totalPublisBook, totalUnPublisBook] = await Promise.all([
         db.book.findMany({
             where: {
                 ...(name && { name: { contains: name, mode: "insensitive" } }),
@@ -81,6 +82,17 @@ const Books = async ({ searchParams }: Props) => {
                 ...(status && { status: status }),
             },
         }),
+        db.book.count(),
+        db.book.count({
+            where: {
+                status: BookStatus.Published,
+            },
+        }),
+        db.book.count({
+            where: {
+                status: BookStatus.Unpublished,
+            },
+        }),
     ]);
 
     const totalPages = Math.ceil(totalBooks / itemsPerPage);
@@ -101,6 +113,42 @@ const Books = async ({ searchParams }: Props) => {
                 </BreadcrumbList>
             </Breadcrumb>
 
+            <div className="grid md:grid-cols-3 gap-4">
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Today Books
+                        </CardTitle>
+                        <BookOpen className="h-4 w-4 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-xl font-bold">{totalbook}</div>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Published Books
+                        </CardTitle>
+                        <BookOpen className="h-4 w-4 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-xl font-bold">{totalPublisBook}</div>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Unpublished Books
+                        </CardTitle>
+                        <BookOpen className="h-4 w-4 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-xl font-bold">{totalUnPublisBook}</div>
+                    </CardContent>
+                </Card>
+            </div>
+
             <Card>
                 <CardHeader>
                     <CardTitle>Books</CardTitle>
@@ -109,7 +157,7 @@ const Books = async ({ searchParams }: Props) => {
                 <CardContent className="space-y-4">
                     <Header />
                     <BookList books={books} />
-                    <CustomPagination totalPages={totalPages} />
+                    <CustomPagination totalCount={totalBooks} />
                 </CardContent>
             </Card>
         </ContentLayout>
